1604294459941eeb7f0c4902bfcfcefccb586e24
Pavel Talaiko <jsdeveloper@yahoo.com>
2018-09-26 12:31:03 +0300

    @PropertySource added.

--- src/main/java/by/psu/StartAttractionProject.java
+++ src/main/java/by/psu/StartAttractionProject.java
@@ -8,10 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-
+@PropertySource("classpath*:static/email_order.html")
 public class StartAttractionProject extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
