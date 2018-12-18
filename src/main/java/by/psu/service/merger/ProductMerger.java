6b99baac2cce41ac0b8a765c51c56ae73037439a
devpav <Pavel.Talaika@artezio.com>
2018-12-19 01:21:41 +0300

    #11: Added merge list product which solving a conflict duplicate products.

--- src/main/java/by/psu/service/merger/ProductMerger.java
+++ src/main/java/by/psu/service/merger/ProductMerger.java
@@ -5,11 +5,25 @@ import by.psu.model.postgres.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Component
 public class ProductMerger implements BaseMerger<Product> {
 
     @Autowired
     private AbstractNsiMerger<TypeService> typeServiceAbstractNsiMerger;
+
+    public List<Product> merge(List<Product> first, List<Product> second) {
+        List<Product> products = BaseMerger.super.merge(first, second);
+
+        Map<UUID, Product> productMap = products.stream()
+                .filter(Objects::nonNull)
+                .filter(product -> product.getService() != null && product.getService().getId() != null)
+                .collect(Collectors.toMap(product -> product.getService().getId(), product -> product, (x, x1) -> x1));
+
+        return new ArrayList<>(productMap.values());
+    }
 
     @Override
     public Product merge(Product first, Product second) {
